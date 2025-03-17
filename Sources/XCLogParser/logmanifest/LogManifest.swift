c7a778eb113f79d0e598cd9cb019bd9ad0c8c542
Hesham Salman <iron-ham@github.com>
2025-03-17 11:12:45 -0400

    swiftlint autofix'

--- Sources/XCLogParser/logmanifest/LogManifest.swift
+++ Sources/XCLogParser/logmanifest/LogManifest.swift
@@ -25,7 +25,7 @@ public struct LogManifest {
 
     public init() {}
 
-    public func getWithLogOptions(_ logOptions: LogOptions) throws  -> [LogManifestEntry] {
+    public func getWithLogOptions(_ logOptions: LogOptions) throws -> [LogManifestEntry] {
         let logFinder = LogFinder()
         let logManifestURL = try logFinder.findLogManifestWithLogOptions(logOptions)
         let logManifestDictionary = try getDictionaryFromURL(logManifestURL)
@@ -35,27 +35,27 @@ public struct LogManifest {
     public func parse(dictionary: NSDictionary, atPath path: String) throws -> [LogManifestEntry] {
         guard let logs = dictionary["logs"] as? [String: [String: Any]] else {
             throw LogError.invalidLogManifest("The file at \(path) is not a valid " +
-                "LogManifest file.")
+                                              "LogManifest file.")
         }
         return try logs.compactMap { entry -> LogManifestEntry? in
-            guard
-                let fileName = entry.value["fileName"] as? String,
-                let title = entry.value["title"] as? String,
-                let uniqueIdentifier = entry.value["uniqueIdentifier"] as? String,
-                let scheme = entry.value["schemeIdentifier-schemeName"] as? String,
-                let timeStartedRecording = entry.value["timeStartedRecording"] as? Double,
-                let timeStoppedRecording = entry.value["timeStoppedRecording"] as? Double,
-                let className = entry.value["className"] as? String,
-                let type = LogManifestEntryType.buildFromClassName(className),
-                let primaryObservable = entry.value["primaryObservable"] as? [String: Any],
-                let totalNumberOfAnalyzerIssues = primaryObservable["totalNumberOfAnalyzerIssues"] as? Int,
-                let totalNumberOfErrors = primaryObservable["totalNumberOfErrors"] as? Int,
-                let totalNumberOfWarnings = primaryObservable["totalNumberOfWarnings"] as? Int,
-                let totalNumberOfTestFailures = primaryObservable["totalNumberOfTestFailures"] as? Int,
-                let highLevelStatus = primaryObservable["highLevelStatus"] as? String
-                else {
-                    throw LogError.invalidLogManifest("The file at \(path) is not a valid " +
-                        "LogManifest file.")
+            guard let fileName = entry.value["fileName"] as? String,
+                  let title = entry.value["title"] as? String,
+                  let uniqueIdentifier = entry.value["uniqueIdentifier"] as? String,
+                  let scheme = entry.value["schemeIdentifier-schemeName"] as? String,
+                  let timeStartedRecording = entry.value["timeStartedRecording"] as? Double,
+                  let timeStoppedRecording = entry.value["timeStoppedRecording"] as? Double,
+                  let className = entry.value["className"] as? String,
+                  let type = LogManifestEntryType.buildFromClassName(className),
+                  let primaryObservable = entry.value["primaryObservable"] as? [String: Any],
+                  let totalNumberOfAnalyzerIssues = primaryObservable["totalNumberOfAnalyzerIssues"] as? Int,
+                  let totalNumberOfErrors = primaryObservable["totalNumberOfErrors"] as? Int,
+                  let totalNumberOfWarnings = primaryObservable["totalNumberOfWarnings"] as? Int,
+                  let totalNumberOfTestFailures = primaryObservable["totalNumberOfTestFailures"] as? Int,
+                  let highLevelStatus = primaryObservable["highLevelStatus"] as? String
+            else {
+                throw LogError.invalidLogManifest(
+                    "The file at \(path) is not a valid LogManifest file."
+                )
             }
             let startDate = Date(timeIntervalSinceReferenceDate: timeStartedRecording)
             let endDate = Date(timeIntervalSinceReferenceDate: timeStoppedRecording)
@@ -68,25 +68,27 @@ public struct LogManifest {
                 totalNumberOfTestFailures: totalNumberOfTestFailures,
                 totalNumberOfWarnings: totalNumberOfWarnings
             )
-            return LogManifestEntry(uniqueIdentifier: uniqueIdentifier,
-                                    title: title,
-                                    scheme: scheme,
-                                    fileName: fileName,
-                                    timestampStart: timestampStart,
-                                    timestampEnd: timestampEnd,
-                                    duration: timestampEnd - timestampStart,
-                                    type: type,
-                                    statistics: statistics
+            return LogManifestEntry(
+                uniqueIdentifier: uniqueIdentifier,
+                title: title,
+                scheme: scheme,
+                fileName: fileName,
+                timestampStart: timestampStart,
+                timestampEnd: timestampEnd,
+                duration: timestampEnd - timestampStart,
+                type: type,
+                statistics: statistics
             )
-            }.sorted(by: { lhs, rhs -> Bool in
-                return lhs.timestampStart > rhs.timestampStart
-            })
+        }.sorted(by: { lhs, rhs -> Bool in
+            return lhs.timestampStart > rhs.timestampStart
+        })
     }
 
     private func getDictionaryFromURL(_ logManifestURL: URL) throws -> NSDictionary {
         guard let logContents = NSDictionary(contentsOfFile: logManifestURL.path) else {
-            throw LogError.invalidLogManifest("The file at \(logManifestURL.path) is not a valid " +
-                "LogManifest file.")
+            throw LogError.invalidLogManifest(
+                "The file at \(logManifestURL.path) is not a valid LogManifest file."
+            )
         }
         return logContents
     }
